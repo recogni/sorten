fcf644b4763f1804fdce2712b3296d1faf353273
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2017-10-15 18:49:36 -0700

    Fully functioning HDR -> JPEG conversion, [local|bucket]->[local|bucket]
    1.  Fix job queue so it uses a non-buffered channel.
    2.  Vendor deps for gcloud, working upload to bucket from local hdr dump
    3.  Move iterator fetching to under the bucket manager
    4.  Implement bucket -> bucket translation using local machine!!

---
+++ bucket.go
@@ -0,0 +1,134 @@
+package main
+
+////////////////////////////////////////////////////////////////////////////////
+
+import (
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/net/context"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+func isBucketPath(s string) bool {
+	return strings.HasPrefix(strings.ToLower(s), "gs://")
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type bucketPath struct {
+	bp      string
+	bucket  string
+	subpath string
+}
+
+func newBucketPath(s string) (*bucketPath, error) {
+	if !isBucketPath(s) {
+		return nil, errors.New("bucket paths should begin with gs://")
+	}
+
+	items := []string{}
+	for _, item := range strings.Split(s[5:], string(filepath.Separator)) {
+		if item != "" {
+			items = append(items, item)
+		}
+	}
+
+	if len(items[0]) == 0 {
+		return nil, errors.New("bucket name not specified")
+	}
+
+	return &bucketPath{
+		bp:      s,
+		bucket:  items[0],
+		subpath: strings.Join(items[1:], string(filepath.Separator)),
+	}, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type bucketManager struct {
+	bucket string
+	ctxt   context.Context
+	client *storage.Client
+}
+
+func newBucketManager(bucketName string) (*bucketManager, error) {
+	ctxt := context.Background()
+	client, err := storage.NewClient(ctxt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &bucketManager{
+		bucket: bucketName,
+		ctxt:   ctxt,
+		client: client,
+	}, nil
+}
+
+func (bm *bucketManager) bucketUpload(dst *bucketPath, src string) error {
+	f, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	wc := bm.client.Bucket(dst.bucket).Object(dst.subpath).NewWriter(bm.ctxt)
+	if _, err := io.Copy(wc, f); err != nil {
+		return err
+	}
+
+	return wc.Close()
+}
+
+func (bm *bucketManager) bucketDownload(dst string, src *bucketPath) error {
+	rc, err := bm.client.Bucket(src.bucket).Object(src.subpath).NewReader(bm.ctxt)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(f, rc); err != nil {
+		return err
+	}
+
+	return rc.Close()
+}
+
+func (bm *bucketManager) getBucketIterator(prefix string) (*storage.ObjectIterator, error) {
+	return bm.client.Bucket(bm.bucket).Objects(bm.ctxt, &storage.Query{Prefix: prefix}), nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+var managers = map[string]*bucketManager{}
+var managerMutex sync.RWMutex
+
+func getBucketManager(bn string) *bucketManager {
+	if bm, ok := managers[bn]; ok {
+		return bm
+	}
+
+	managerMutex.Lock()
+	defer managerMutex.Unlock()
+
+	bm, err := newBucketManager(bn)
+	fatalOnErr(err)
+
+	managers[bn] = bm
+	return bm
+}
+
+////////////////////////////////////////////////////////////////////////////////
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -16,6 +17,9 @@ import (
 	"runtime"
 	"strings"
 	"sync"
+
+	"github.com/burger/goterm"
+	"google.golang.org/api/iterator"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -26,10 +30,66 @@ var (
 		outputDir      string
 		magickBins     string
 		useImageMagick bool
+		numCpus        int
 	}{}
 
 	gDebugPrintsEnabled = true
 )
+
+////////////////////////////////////////////////////////////////////////////////
+
+// workerUpdate contains a worker index and a message being sent from the
+// worker to the main thread.
+type workerUpdate struct {
+	id  int
+	msg string
+}
+
+// repaintUpdates waits on all specified workers and repaints the progress
+// string per worker to stdout.
+func repaintUpdates(workerCount int, updates <-chan *workerUpdate) {
+	header := "Application starting up ..."
+	us := make([]string, workerCount)
+	for i := 0; i < workerCount; i++ {
+		us[i] = fmt.Sprintf("initializing ...")
+	}
+
+	repaintCounter := 0
+	repaint := func() {
+		goterm.Clear()
+		defer goterm.Flush()
+
+		goterm.MoveCursor(1, 1)
+		goterm.Printf("%s\n", header)
+		for i := 0; i < workerCount; i++ {
+			goterm.MoveCursor(1, i+2)
+			goterm.Printf("worker %02d :: %s\n", i, us[i])
+		}
+		repaintCounter += 1
+	}
+
+	repaint()
+	for update := range updates {
+		if update.id == workerCount {
+			header = update.msg
+		} else if update.id >= 0 && update.id < workerCount {
+			us[update.id] = update.msg
+		}
+		repaint()
+		// time.Sleep(32 * time.Millisecond)
+	}
+}
+
+func setUpdate(updates chan<- *workerUpdate, index int, format string, a ...interface{}) {
+	updates <- &workerUpdate{
+		id:  index,
+		msg: fmt.Sprintf(format, a...),
+	}
+}
+
+func setStatus(updates chan<- *workerUpdate, format string, a ...interface{}) {
+	setUpdate(updates, CLI.numCpus, format, a...)
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -43,16 +103,77 @@ type hdrToJpegJob struct {
 // hdrToJpegWorker is a specialized worker which will run imagemagick on one
 // goroutine to convert the source file into the appropriate destination
 // file / format (as specified by the destination file's extension).
-func hdrToJpegWorker(workerId int, jobs <-chan *hdrToJpegJob, wg *sync.WaitGroup) {
+func hdrToJpegWorker(workerId int, jobs <-chan *hdrToJpegJob, updates chan<- *workerUpdate, wg *sync.WaitGroup) {
+	setUpdate(updates, workerId, "is ready for work")
 	for job := range jobs {
-		cmd := exec.Command(path.Join(CLI.magickBins, "convert"), job.source, job.destination)
+		source := job.source
+		if isBucketPath(job.source) {
+			source = path.Join(os.TempDir(), fmt.Sprintf("worker_%d_input.hdr", workerId))
+			bp, _ := newBucketPath(job.source)
+			bm := getBucketManager(bp.bucket)
+
+			setUpdate(updates, workerId, "attempting to download %s -> %s", job.source, source)
+			if err := bm.bucketDownload(source, bp); err != nil {
+				setUpdate(updates, workerId, "Warning: unable to download %s from bucket, error: %s", job.source, err.Error())
+			} else {
+				setUpdate(updates, workerId, "download successful!")
+			}
+		}
+
+		isBucketDst := false
+		destination := job.destination
+		if isBucketPath(job.destination) {
+			isBucketDst = true
+			destination = path.Join(os.TempDir(), fmt.Sprintf("worker_%d_output.jpeg", workerId))
+		}
+
+		setUpdate(updates, workerId, "converting %s -> %s", source, destination)
+		cmd := exec.Command(path.Join(CLI.magickBins, "convert"), source, destination)
 		if _, err := cmd.CombinedOutput(); err != nil {
-			log.Printf("Warning: `convert %s %s` had error: %s", job.source, job.destination, err.Error())
+			setUpdate(updates, workerId, "Warning: `convert %s %s` had error: %s", source, destination, err.Error())
+		} else {
+			setUpdate(updates, workerId, "conversion successful!")
 		}
-		if gDebugPrintsEnabled {
-			log.Printf("Wrote file : %s\n", job.destination)
+
+		if isBucketDst {
+			setUpdate(updates, workerId, "uploading %s to google bucket %s", destination, job.destination)
+			bp, _ := newBucketPath(job.destination)
+			bm := getBucketManager(bp.bucket)
+
+			if err := bm.bucketUpload(bp, destination); err != nil {
+				setUpdate(updates, workerId, "Error: %s\n", err.Error())
+			} else if gDebugPrintsEnabled {
+				setUpdate(updates, workerId, "upload successful!")
+			}
 		}
+
+		// Job done, tag the workgroup and check for more work.
 		wg.Done()
+		setUpdate(updates, workerId, "is now idle")
+	}
+}
+
+// queueJobFiltered checks the given file for the appropriate extension, and if
+// it matches queues the current file as work for the next available worker queue.
+func queuqJobFiltered(fp string, jobs chan *hdrToJpegJob, wg *sync.WaitGroup) {
+	if strings.ToLower(path.Ext(fp)) == ".hdr" {
+		d, fn := filepath.Dir(fp), filepath.Base(fp)
+		rd, err := filepath.Rel(CLI.inputDir, d)
+		if err != nil {
+			// TODO: Handle error case if we can open a file!
+		} else {
+			dstfile := fn[0:len(fn)-len(filepath.Ext(fn))] + ".jpeg"
+			dpitems := []string{CLI.outputDir}
+			if rd != "." {
+				dpitems = append(dpitems, []string{rd, dstfile}...)
+			} else {
+				dpitems = append(dpitems, dstfile)
+			}
+			dp := strings.Join(dpitems, string(filepath.Separator))
+
+			wg.Add(1)
+			jobs <- &hdrToJpegJob{source: fp, destination: dp}
+		}
 	}
 }
 
@@ -67,50 +188,57 @@ func fatalOnErr(err error) {
 
 // main entry point.
 func main() {
-	log.Printf("Using %d CPU cores ...\n", runtime.NumCPU())
-
-	// primitive to wait on all jobs finishing before terminating the app.
 	var wg sync.WaitGroup
+	jobs := make(chan *hdrToJpegJob)
 
-	log.Printf("Building file list ... (this can take a while on large mounted dirs) ...\n")
-	files := []string{}
-	fatalOnErr(filepath.Walk(CLI.inputDir, func(fp string, fi os.FileInfo, err error) error {
-		if err != nil {
-			log.Printf("Warning found error walking dir. Error: %s\n", err.Error())
-		} else {
-			if strings.ToLower(path.Ext(fp)) == ".hdr" {
-				if gDebugPrintsEnabled {
-					log.Printf("Found file : %s\n", fp)
-				}
-				files = append(files, fp)
-			}
-		}
-		return nil
-	}))
+	// Make a channel to get updates from the workers so we can aggregate
+	// messages nicelyer.
+	updates := make(chan *workerUpdate)
+	go repaintUpdates(CLI.numCpus, updates)
 
-	// Create a buffered channel to feed work, length is worst case size.
-	jobs := make(chan *hdrToJpegJob, len(files))
+	setStatus(updates, "Using %d CPU cores ...", CLI.numCpus)
 
 	// Create the workers based on how many CPU cores the system has.
-	for wId := 0; wId < runtime.NumCPU(); wId++ {
-		go hdrToJpegWorker(wId, jobs, &wg)
+	for wId := 0; wId < CLI.numCpus; wId++ {
+		go hdrToJpegWorker(wId, jobs, updates, &wg)
 	}
 
-	// Wait-group worst case depth
-	wg.Add(len(files))
+	if isBucketPath(CLI.inputDir) {
+		bp, err := newBucketPath(CLI.inputDir)
+		fatalOnErr(err)
 
-	// Iterate over file list and queue them for processing.
-	jobCount := 0
-	for _, fp := range files {
-		d, fn := filepath.Dir(fp), filepath.Base(fp)
-		rd, err := filepath.Rel(CLI.inputDir, d)
-		if err != nil {
-			log.Printf("Error getting relative path: %s\n", err.Error())
-		} else {
-			dp := path.Join(CLI.outputDir, rd, fn[0:len(fn)-len(filepath.Ext(fn))]+".jpeg")
-			jobs <- &hdrToJpegJob{source: fp, destination: dp}
-			jobCount += 1
+		setStatus(updates, "Using google cloud APIs to access bucket: %s", bp.bucket)
+		bm := getBucketManager(bp.bucket)
+
+		it, err := bm.getBucketIterator(bp.subpath)
+		fatalOnErr(err)
+
+		c := 0
+		for {
+			attrs, err := it.Next()
+			if err == iterator.Done {
+				break
+			} else {
+				fatalOnErr(err)
+			}
+
+			fp := "gs://" + strings.Join([]string{bp.bucket, attrs.Name}, string(filepath.Separator))
+			queuqJobFiltered(fp, jobs, &wg)
+
+			c += 1
+			setStatus(updates, "found %d files so far ...", c)
 		}
+
+	} else {
+		setStatus(updates, "Building file list ... (this can take a while on large mounted dirs) ...")
+		fatalOnErr(filepath.Walk(CLI.inputDir, func(fp string, fi os.FileInfo, err error) error {
+			if err != nil {
+				setStatus(updates, "Warning found error walking dir. Error: %s", err.Error())
+			} else {
+				queuqJobFiltered(fp, jobs, &wg)
+			}
+			return nil
+		}))
 	}
 
 	// We are done feeding work to the workers, close the jobs channel so that
@@ -118,7 +246,9 @@ func main() {
 	close(jobs)
 
 	wg.Wait()
-	log.Printf("Successfully converted %d files.\n", jobCount)
+
+	setStatus(updates, "Done!")
+	close(updates)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -141,13 +271,17 @@ func init() {
 		log.Fatalf("specify output directory with --output")
 	}
 
-	if _, err := os.Stat(CLI.outputDir); os.IsNotExist(err) {
-		log.Fatalf("output directory (%s) does not exist!", CLI.outputDir)
+	if !isBucketPath(CLI.outputDir) {
+		if _, err := os.Stat(CLI.outputDir); os.IsNotExist(err) {
+			log.Fatalf("output directory (%s) does not exist!", CLI.outputDir)
+		}
 	}
 
 	if _, err := os.Stat(path.Join(CLI.magickBins, "convert")); os.IsNotExist(err) {
 		log.Fatalf("Imagemagick not correctly installed! convert utility missing!")
 	}
+
+	CLI.numCpus = runtime.NumCPU()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
